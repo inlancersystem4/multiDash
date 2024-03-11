e78777c747c36463facd0d90a3215f8246231c05
inlancersystem4 <inlancersystem4@gmail.com>
2024-03-11 10:37:56 +0530

    add new chat project in vue js

---
+++ chat/src/views/chat.vue
@@ -0,0 +1,24 @@
+<script>
+
+const socket = new WebSocket('http://127.0.0.1:3001');
+
+export default {
+
+    created() {
+        console.log(socket)
+
+        // socket.onopen(){
+        //     console.log("WebSocket connection opened:", event);
+        // };
+        // socket.onmessage => (event) {
+        //     console.log("WebSocket message received:", event.value);
+        // };
+    },
+}
+</script>
+
+<template>
+    <div>
+        121212
+    </div>
+</template>
